6fa5950a1b6eafcaa16f24092cb712be493ac7ac
Shivakumar1090 <ch.shivakumar491@gmail.com>
2024-07-08 10:55:03 +0530

    gmap width update

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -48,7 +48,7 @@ const Contactus = () => {
                         <iframe 
                             title='company location'
                             src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3779.9975131577103!2d73.9443448751952!3d18.664107582457568!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDM5JzUwLjgiTiA3M8KwNTYnNDguOSJF!5e0!3m2!1sen!2sin!4v1720370958455!5m2!1sen!2sin" 
-                            width="600" 
+                            width="100%" 
                             height="450" 
                             style={{border:"0",}}
                             allowfullscreen="" 
